a2e4d45f25a169e54f4a55990b9839cac3653b0b
tokumshy <tokumshy@fit.cvut.cz>
2023-12-05 17:24:32 +0100

    Update file reverse-tree.rkt

--- reverse-tree.rkt
+++ reverse-tree.rkt
@@ -1,8 +1,12 @@
+#lang racket
+
 (define (reverse-tree bst)
-  (cond
-    ((null? bst) '())
-    (else
-     (list (car bst)            
-           (reverse-tree (caddr bst))
-           (reverse-tree (cadr bst)))))
-  )
+  (if (null? bst)                            
+      '()
+      (let ((root (car bst))                 
+            (left (cadr bst))
+            (right (caddr bst)))
+        (list root                           
+              (reverse-tree right)           
+              (reverse-tree left))))         
+    )
