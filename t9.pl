869a2bab0a2e6021d9a856da3b5ad756b4fd01d4
tokumshy <tokumshy@fit.cvut.cz>
2023-12-18 16:32:45 +0100

    Update file t9.pl

--- t9.pl
+++ t9.pl
@@ -8,13 +8,19 @@ digit_to_let(8, [t,u,v]).
 digit_to_let(9, [w,x,y,z]).
 
 t9(Number, Word) :-
-    number_codes(Number, Digits),
-    maplist(digit_to_let, Digits, Word).
+    number_digits(Number, Digits),
+    maplist(digit_to_letters, Digits, LettersLists),
+    maplist(member, Word, LettersLists).
 
-digit_to_letter(DigitCode, Letter) :-
-    CodeOffset is DigitCode - 48,
-    digit_to_let(CodeOffset, Letters),
-    member(Letter, Letters).
+number_digits(Number, Digits) :-
+    number_codes(Number, Codes),
+    maplist(code_to_digit, Codes, Digits).
+
+code_to_digit(Code, Digit) :-
+    Digit is Code - 48.
+
+digit_to_letters(Digit, Letters) :-
+    digit_to_let(Digit, Letters).
 
 t9(Number, _) :-
     (Number == 0; Number == 1),
