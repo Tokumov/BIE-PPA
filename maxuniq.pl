cf0a8ee79d1c9f4ceaffaef4a9981efcc0d6bd28
tokumshy <tokumshy@fit.cvut.cz>
2023-12-19 19:09:54 +0100

    Update file maxuniq.pl

---
+++ maxuniq.pl
@@ -0,0 +1,25 @@
+maxuniq(Seq, Result) :-
+    findall(SubSeq, divide(Seq, SubSeq), AllSubSeqs),
+    searchMaxDistinctSubSeqs(AllSubSeqs, 0, Result).
+
+searchMaxDistinctSubSeqs([], MaxLen, MaxLen).
+searchMaxDistinctSubSeqs([First|Rest], CurrMaxLen, MaxLen) :-
+    length(First, Len),
+    (checkDistinct(First) -> 
+        (Len > CurrMaxLen -> UpdatedMaxLen = Len; UpdatedMaxLen = CurrMaxLen)
+    ; UpdatedMaxLen = CurrMaxLen),
+    searchMaxDistinctSubSeqs(Rest, UpdatedMaxLen, MaxLen).
+
+checkDistinct(Seq) :-
+    sort(Seq, SortedSeq),
+    length(Seq, OrigLen),
+    length(SortedSeq, SortedLen),
+    OrigLen == SortedLen.
+
+divide([], []).
+divide([Head|Tail], [[Head|Tail2]|Other]) :-
+    append(Tail2, Remain, Tail),
+    divide(Remain, Other).
+
+divide([Head|Tail], [[Head]|Other]) :-
+    divide(Tail, Other).
